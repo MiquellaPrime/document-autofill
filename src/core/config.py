1cddca99b5c10985f522a1b323ba90c0c0a023a8
Mykhailo Popudniak <dev.miquella@gmail.com>
2025-12-20 19:27:21 +0200

    feat(config): add database config

--- src/core/config.py
+++ src/core/config.py
@@ -7,6 +7,32 @@ class ServerConfig(BaseModel):
     port: int = 8000
 
 
+class PostgresConfig(BaseModel):
+    host: str
+    port: int
+    user: str
+    password: str
+    dbname: str
+
+    @property
+    def async_dsn(self) -> str:
+        return f"postgresql+asyncpg://{self.user}:{self.password}@{self.host}:{self.port}/{self.dbname}"
+
+
+class DatabaseConfig(BaseModel):
+    postgres: PostgresConfig
+
+    @property
+    def url(self) -> str:
+        return self.postgres.async_dsn
+
+    echo: bool = False
+    max_overflow: int = 30
+    pool_size: int = 20
+    pool_pre_ping: bool = True
+    pool_recycle: int = 3600
+
+
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         case_sensitive=False,
@@ -16,6 +42,8 @@ class Settings(BaseSettings):
     )
 
     server: ServerConfig = ServerConfig()
+
+    db: DatabaseConfig
 
 
 settings = Settings()  # type: ignore
