8fb1acd8632f1c35ad52b72440f7e184deef4768
Mykhailo Popudniak <dev.miquella@gmail.com>
2025-12-27 14:09:24 +0200

    feat(app): add static files and template rendering
    Mounted the /static route to serve static files and set up Jinja2 template rendering for the home page at '/'. This enables serving static assets and rendering HTML templates using FastAPI.

--- src/main.py
+++ src/main.py
@@ -1,9 +1,25 @@
 import uvicorn
+from fastapi import Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 from src.core import settings
 from src.create_app import create_app
 
+templates = Jinja2Templates(directory="src/templates")
+
 app = create_app()
+
+app.mount(
+    "/static",
+    StaticFiles(directory="src/static"),
+    name="static",
+)
+
+
+@app.get("/")
+async def home_page(request: Request):
+    return templates.TemplateResponse(request, name="index.html")
 
 
 if __name__ == "__main__":
